a0e397e8a4a4d637ee0ded7f04974e85b9125a5e
Jordan James Holt <jordholt@gmail.com>
2014-03-20 01:31:37 -0400

    Code cleanup and preparations
    Cleaned up code and file system in preparation for implementations to
    integrate to Parse.

--- scripts/serverComparator.js
+++ scripts/serverComparator.js
@@ -2,22 +2,7 @@
 
 var serverComparator = {
 	data : {
-		getWorlds : function(location) {
-			return $.ajax({
-				url:"https://api.guildwars2.com/v1/world_names.json?lang=" + location,
-				dataType:"JSON",
-				type:"GET"
-			});
-		},
-
-		getWorldEventStatus : function(worldID) {
-			return $.ajax({
-				url:"https://api.guildwars2.com/v1/events.json?world_id=" + worldID,
-				dataType:"JSON",
-				type:"GET"
-			});
-		},
-
+		// Change this function to obtain data through parse and update the array via long polling.
 		getWorldData : function(location) {
 			serverComparator.data.getWorlds(location).done(function(world) {
 
@@ -29,48 +14,6 @@ var serverComparator = {
 				ko.applyBindings(serverComparator.model.WorldDataListViewModel);
 
 				serverComparator.data.calculateWorldData(serverComparator.model.WorldDataListViewModel.worldData());
-			});
-		},
-
-		calculateWorldData : function(worldDataList) {
-			var success = 0;
-			var failed = 0;
-			var worldsAveraged = 0;
-			var maxWorlds = worldDataList.length;
-
-
-			$.each(worldDataList, function(i) {
-
-				serverComparator.data.getWorldEventStatus(worldDataList[i].worldID()).done(function(eventStatuses){
-
-					$.each(eventStatuses, function(i) {
-						
-						$.each(eventStatuses[i], function(j) {
-							if(eventStatuses[i][j].state === "Success") {
-								success++;
-							}
-						});
-
-						$.each(eventStatuses[i], function(j) {
-							if(eventStatuses[i][j].state === "Fail") {
-								failed++;
-							}
-						});
-
-					});
-
-					worldDataList[i].percentages.push(serverComparator.utils.getAverageEventSuccess(success,failed));
-
-					worldDataList[i].successPercentage((serverComparator.utils.getSum(worldDataList[i].percentages()) / worldDataList[i].percentages().length).toFixed(2));
-					
-					success = 0;
-					worldsAveraged++;
-
-					if(worldsAveraged === maxWorlds) {
-						serverComparator.data.calculateWorldData(worldDataList);
-					}
-				});
-				
 			});
 		}
 	},
@@ -85,20 +28,6 @@ var serverComparator = {
 			self.worldName = ko.observable(worldName);
 			self.successPercentage = ko.observable(successPercentage);
 			self.percentages = ko.observableArray();
-		}
-	},
-	utils : {
-		getSum : function(list) {
-			var total = 0;
-
-			for(var i = 0; i < list.length; i++) {
-				total += list[i];
-			}
-
-			return total;
-		},
-		getAverageEventSuccess : function(successfulEvents, failedEvents) {
-			return successfulEvents / (successfulEvents + failedEvents);
 		}
 	}
 }
