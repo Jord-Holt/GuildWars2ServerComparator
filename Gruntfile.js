ca957476a2a2ee755fc36af3842de89d0682267c
Jordan James Holt <jordholt@gmail.com>
2014-05-26 01:57:47 -0400

    Cleaning up source code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		gitclone: {
+		gitclone : {
 			clone : {
 				options: {
 					repository : 'https://github.com/Xfanger/GuildWars2ServerComparator.git',
@@ -11,16 +11,32 @@ module.exports = function(grunt) {
 		},
 		copy: {
 			main: {
-				src: 'Build/*',
-				dest: '../parse/public/'
+				src: ['Build/*'],
+				dest: 'parse/public/'
+			},
+			options : {
+				noProcess : ["Build/.git","Build/.gitattributes", "Build/.gitignore"] 
 			}
 		},
-		clean : ["Build/*"]
+		clean : ["Build/*","Build/.git","Build/.gitattributes", "Build/.gitignore"],
+		connect : {
+			server : {
+				options : {
+					port : 9001,
+					keepalive : true
+					base : "/GuildWars2ServerComparator"
+				}
+			}
+		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-git');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-git');
 
-	grunt.registerTask('default', ['clean','gitclone','copy']);
+	grunt.registerTask('default', []);
+	grunt.registerTask('stage', ['clean','gitclone','copy']); // Will pull latest changes from project master branch and stage in parse directory for deployment to test environment.
+	grunt.registerTask('start-dev', ['connect']); // Spin up quick server to view application locally.
 };
